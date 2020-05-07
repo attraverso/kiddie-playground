08166972d76c17f4912c8b08d78babf3a86471bb
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-07 13:46:05 +0200

    UTTT: player differentiation via click/dblclick [has issues]

--- ultimate-tic-tac-toe/main.js
+++ ultimate-tic-tac-toe/main.js
@@ -1,8 +1,27 @@
 /*DNT*/$(document).ready(function() {/*DNT*/
 
-for (var i = 0; i < 81; i++) {
-  $('#game').append('<div class="box"></div>');
+/*STEP generate game field via jQuery*/
+/*generate outer grid*/
+for (var i = 0; i < 9; i++) {
+  $('#game').append('<div class="gridbox"></div>');
 };
+ /*generate innner grids*/
+for (var i = 0; i < 9; i++) {
+  $('.gridbox').append('<div class="box"></div>');
+};
+
+/*STEP apply color by player*/
+/*player 1 = click - player 2 = double click*/
+/*BB HAS ISSUES */
+
+$('.box').click(function() {
+  $(this).addClass('blue');
+})
+
+$('.box').dblclick(function() {
+  $(this).addClass('red');
+  $(this).removeClass('blue');
+})
 
 
 
